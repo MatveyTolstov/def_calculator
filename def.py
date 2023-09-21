045b57b5049a0ee06c1bf363332064b7d872e7b2
MatveyTolstov <isip_m.a.tolstov@mpt.ru>
2023-09-21 11:22:42 +0300

    Create def.py

---
+++ def.py
@@ -0,0 +1,70 @@
+import math 
+o = (input("Введите операцию"))
+if o == "+":
+    a = float (input ("Введите первое число"))
+    b = float (input ("Введите второе  число"))
+    def sum (a,b):
+        print (a + b)
+        return a + b 
+    sum (a, b )
+elif o == "-":
+    a = float (input ("Введите первое число"))
+    b = float (input ("Введите второе  число"))
+    def minus (a,b):
+        print(a - b)
+        return (a - b)
+
+    minus (a, b)
+elif o == "*":
+    a = float (input ("Введите первое число"))
+    b = float (input ("Введите второе число"))
+    def umnoj (a,b):
+        print (a * b)
+        return a * b
+    umnoj (a,b)
+elif o == "/":
+    a = float (input ("Введите первое число"))
+    b = float (input ("Введите второе число"))
+    def dele (a,b):
+        print (a / b)
+        return a / b
+    dele (a,b)
+elif o == "**":
+    a = float (input ("Введите первое число"))
+    b = float (input ("Введите второе число"))  
+    def step (a,b):
+        print (a ** b)
+        return a ** b
+    step (a,b)
+elif o == "кор":
+    a = float (input ("Введите первое число"))
+    def cor (a):
+        print (a ** 0.5)
+        return a ** 0.5
+    cor (a)
+elif o == "син":
+    a = float (input ("Введите первое число"))
+    def sin (a):
+        print (math.sin (a))
+        return math.sin (a)
+    sin (a)
+elif o == "кос":
+    a = float (input ("Введите первое число"))
+    def cos (a):
+        print (math.cos  (a))
+        return math.cos (a)
+    cos (a)
+elif o == "таг":
+    a = float (input ("Введите первое число"))
+    def tag (a):
+        print(math.tan (a))
+        return math.tan (a)
+    tag (a)
+elif o == "фак":
+    a= float (input ("Введите первое число"))
+    def fac (a):
+        print (math.factorial (a))
+        return math.factorial (a)
+    fac (a)
+else:
+    print ("Ошибка")
